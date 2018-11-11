c47d3a5bab6956409489852743f881f685ee8ccc
jmj <1176372741@qq.com>
2018-11-11 22:37:50 +0800

    分页显示品牌列表功能

--- pingyougou-sellergoods-interface/src/main/java/com/pinyougou/sellergoods/service/BrandService.java
+++ pingyougou-sellergoods-interface/src/main/java/com/pinyougou/sellergoods/service/BrandService.java
@@ -4,6 +4,8 @@ import java.util.List;
 
 import com.pinyougou.pojo.TbBrand;
 
+import entity.PageResult;
+
 /***
  * 品牌类接口
  * @author Administrator
@@ -14,7 +16,15 @@ public interface BrandService {
 	/**
 	 * 展示所有的品牌
 	 * 此接口提供给controller
-	 * @return
+	 * @return  所有品牌的集合
 	 */
 	List<TbBrand> findAll();
+	
+	/**
+	 * 分页显示品牌列表
+	 * @param currentPage  当前页
+	 * @param rows 每页所显示的记录的条数
+	 * @return
+	 */
+	PageResult findPage(int currentPage,int rows);
 }
--- pingyougou-sellergoods-service/src/main/java/com/pingyougou/sellergoods/service/impl/BrandServiceImpl.java
+++ pingyougou-sellergoods-service/src/main/java/com/pingyougou/sellergoods/service/impl/BrandServiceImpl.java
@@ -5,9 +5,13 @@ import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 
 import com.alibaba.dubbo.config.annotation.Service;
+import com.github.pagehelper.Page;
+import com.github.pagehelper.PageHelper;
 import com.pinyougou.mapper.TbBrandMapper;
 import com.pinyougou.pojo.TbBrand;
 import com.pinyougou.sellergoods.service.BrandService;
+
+import entity.PageResult;
 
 @Service
 public class BrandServiceImpl implements BrandService {
@@ -23,6 +27,20 @@ public class BrandServiceImpl implements BrandService {
 	public List<TbBrand> findAll() {
 		
 		return tbBrandMapper.selectByExample(null);
+	}
+
+	/***
+	 * 分页显示品牌列表
+	 * currentPage 当前页码
+	 * rows 每页显示的条数
+	 */
+	@Override
+	public PageResult findPage(int currentPage, int rows) {
+		PageHelper.startPage(currentPage,rows);
+		Page<TbBrand> page = (Page<TbBrand>) tbBrandMapper.selectByExample(null);
+		System.out.println("serviceImpl总页数:"+page.getTotal());    //获得总页数
+		System.out.println("serviceImpl记录:"+page.getResult());   //获得当前页面所需要显示的记录数
+		return new PageResult(page.getTotal(),page.getResult());
 	}
 
 }
